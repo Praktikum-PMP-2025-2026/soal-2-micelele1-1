e6260487cebc6c2dda6b44f5c1101f77c46f8e94
micelele1 <michelleyeo36@gmail.com>
2026-05-04 09:41:28 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -8,8 +8,12 @@
  *                          tetapi semua elemen dari kedua list tetap muncul sekali dalam output.
  */
 
- #include <stdio.h>
- #include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+#include <stdbool.h>
+#define nullptr ((void*)0)
 
  //algoritma merge menggunakan referensi dari geeksforgeeks dengan tautan berikut https://www.geeksforgeeks.org/dsa/merge-two-sorted-linked-lists/
  struct Node {
@@ -55,17 +59,60 @@
         if (curr->next != NULL) {
             printf(" ");
         }
-        curr = curr->next;
-    }
+        curr = curr->next;}
     }
 }
 
+
+
  int main(){
-    struct Node *head1= NULL;
-    struct Node *head2= NULL;
+    int input;
+    int ukuran1;
+    int ukuran2;
+    int i;
+    if (input == '\n'){
+        return 1;}
+    else{
+        struct Node* head1 = nullptr;
+        for(i=0; i<ukuran1; i++) {
+            struct Node *newnode1 = createNode(input);
+            newnode1->next=nullptr;
+        if(head1==nullptr){
+            head1==newnode1;
+        }
+        else{
+            struct Node *temp1 = head1;
+            while(temp1->next!=nullptr){
+                temp1 = temp1->next;
+            }
+            temp1->next=newnode1;
+        }       
+       
+    } 
+    i++;
+
+    if(i=ukuran1){
+            ukuran2=input;
+        }
+    struct Node* head2 = nullptr;
+    for(int i=ukuran1; i<ukuran2+ukuran1; i++){
+            struct Node *newnode2 = createNode(input);
+            newnode2->next=nullptr;
+            if(head2==nullptr){
+            head2==newnode2;
+        }
+        else{
+            struct Node *temp2 = head2;
+            while(temp2->next!=nullptr){
+                temp2 = temp2->next;
+            }
+            temp2->next=newnode2;
+        }
+        }
     struct Node *res = sortedMerge(head1, head2);
-    
     printlist(res);
+    }
+    
     return 0;
 
  }
