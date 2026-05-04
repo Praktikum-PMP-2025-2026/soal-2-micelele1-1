183aaee7ad13aa3de0862e3957f9f935324aec81
micelele1 <michelleyeo36@gmail.com>
2026-05-04 10:18:22 +0700

    Refactor linked list implementation and fix NULL usage
    Refactor code to use NULL instead of nullptr and fix various print statements. Adjust linked list creation logic and ensure proper handling of input.

--- soal2.c
+++ soal2.c
@@ -13,7 +13,6 @@
 #include<stdlib.h>
 #include<string.h>
 #include <stdbool.h>
-#define nullptr ((void*)0)
 
  //algoritma merge menggunakan referensi dari geeksforgeeks dengan tautan berikut https://www.geeksforgeeks.org/dsa/merge-two-sorted-linked-lists/
  struct Node {
@@ -25,11 +24,12 @@
     struct Node *newNode= (struct Node*)malloc (sizeof(struct Node));
     newNode-> data = data;
     newNode-> next = NULL;
-    printf("new node %d\n", newNode->data);
+    printf("data newnode %d", newNode->data);
+    printf("pointer newnode %d", newNode->next->data);
     return newNode;
  }
+
  struct Node *sortedMerge(struct Node *head1, struct Node *head2) {
-  
     // base cases
     if (head1 == NULL)
         return head2;
@@ -47,14 +47,12 @@
     }
 }
 
-
-
  void printlist(struct Node *curr) {
     printf("MERGED ");
     if(curr==NULL){
         printf("EMPTY");
     }else{
-        while (curr != NULL) {
+        while (curr!= NULL) {
         printf("%d", curr->data);
         if (curr->next != NULL) {
             printf(" ");
@@ -63,56 +61,56 @@
     }
 }
 
-
-
  int main(){
     int input;
     int ukuran1;
     int ukuran2;
     int i;
-    if (input == '\n'){
-        return 1;}
-    else{
-        struct Node* head1 = nullptr;
+    struct Node* head1 = NULL;
+    struct Node* head2 = NULL;
+    while (scanf("%d", &input)==1){
+        printf("input 1 %d", input);
+        ukuran1=input;
+        printf("ukuran 1 %d", input);
         for(i=0; i<ukuran1; i++) {
             struct Node *newnode1 = createNode(input);
-            newnode1->next=nullptr;
-        if(head1==nullptr){
-            head1==newnode1;
+            newnode1->next=NULL;
+        if(head1==NULL){
+            head1=newnode1;
         }
         else{
             struct Node *temp1 = head1;
-            while(temp1->next!=nullptr){
+            while(temp1->next!=NULL){
                 temp1 = temp1->next;
             }
             temp1->next=newnode1;
-        }       
-       
-    } 
+        }
+    }
     i++;
-
     if(i=ukuran1){
             ukuran2=input;
+            printf("ukuran 2 %d", ukuran2);
         }
-    struct Node* head2 = nullptr;
     for(int i=ukuran1; i<ukuran2+ukuran1; i++){
+        printf("input 2 %d", input);
             struct Node *newnode2 = createNode(input);
-            newnode2->next=nullptr;
-            if(head2==nullptr){
-            head2==newnode2;
+            newnode2->next=NULL;
+            if(head2==NULL){
+            head2=newnode2;
         }
         else{
             struct Node *temp2 = head2;
-            while(temp2->next!=nullptr){
+            while(temp2->next!=NULL){
                 temp2 = temp2->next;
             }
             temp2->next=newnode2;
         }
-        }
+        }       
+       
+    } 
     struct Node *res = sortedMerge(head1, head2);
     printlist(res);
+    return 0;    
     }
     
-    return 0;
 
- }
